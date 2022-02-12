6dc369835dc4c80565499531d54475bfddce9449
dim-kastr <swagmyfamily@icloud.com>
2022-02-12 23:44:03 +0700

    task1.1

--- src/server/routes/v1/events.ts
+++
@@ -1,9 +0,0 @@
-// import { readingContractEventsDeposit } from '../../api/v1/events';
-
-// export default [
-//     {
-//         method: 'GET',
-//         path: '/v1/contract',
-//         handler: readingContractEventsDeposit,
-//     },
-// ];
