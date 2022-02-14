ae648e6ba1fe216a5a2ff3256db748a2d771cf1f
dim-kastr <swagmyfamily@icloud.com>
2022-02-15 00:42:01 +0700

    task1.3

--- src/server/utils/blockchain.ts
+++ src/server/utils/blockchain.ts
@@ -1,8 +1,5 @@
 import { ContractAbi } from '../../abi/contractAbi';
-import { User } from '../models/User';
-import { Wallet } from '../models/Wallet';
-import { BigNumber } from "bignumber.js";
-import { Transaction } from '../models/Transactions';
+import { handlerDeposit, handlerWithdraw } from './events';
 
 const Web3 = require("web3");
 
@@ -40,7 +37,7 @@ export const createContract = async () => {
     return myContract
 }
 
-export const readingContractEventsDeposit = async () => {
+export const readingContractEvents = async () => {
     try {
         const contract = await createContract();
         const events = await contract.getPastEvents('Deposit', {
@@ -49,97 +46,15 @@ export const readingContractEventsDeposit = async () => {
         })
 
         for (let event of events) {
-            const { addressOwner, amount, addressToken } = event.returnValues
-            const amountWallet = Web3.utils.fromWei(amount, 'ether')
 
-            let user = await User.findOne({
-                where: {
-                    addressOwner
-                },
-                include: {
-                    model: Wallet,
-                    as: "wallets",
-                    where: {
-                        addressToken
-                    },
-                    required: false
-                }
-            })
-
-            if (!user) {
-                user = await User.createUser(addressOwner);
+            if (event.event === "Deposit") {
+                await handlerDeposit(event)
             }
 
-            if (!user.wallets || !user.wallets.length) {
-                await Wallet.createWallet({
-                    userId: user.id,
-                    amount: amountWallet,
-                    addressToken
-                })
-            } else {
-                let balanceFullWallet = new BigNumber(user.wallets[0].amount);
-                let amountEvent = new BigNumber(amountWallet);
-                await user.wallets[0].update({
-                    amount: +balanceFullWallet + +amountEvent
-                })
+            if (event.event === "Withdraw") {
+                await handlerWithdraw(event)
             }
+
         }
-    } catch (e) { console.log(e) }
-}
-
-export const readingContractEventsWithdraw = async () => {
-    try {
-        const contract = await createContract();
-        const events = await contract.getPastEvents('Withdraw', {
-            fromBlock: 0,
-            toBlock: 'latest',
-        })
-
-        for (let event of events) {
-            const { account, amount, token } = event.returnValues
-            const amountWallet = Web3.utils.fromWei(amount, 'ether')
-
-            const user = await User.findOne({
-                where: {
-                    addressOwner: account
-                }
-            })
-
-            const wallet = await Wallet.findOne({
-                where: {
-                    userId: user.id,
-                    addressToken: token
-                }
-            })
-
-            let balanceFullWallet = new BigNumber(wallet.amount);
-            let amountEvent = new BigNumber(amountWallet);
-            wallet.update({
-                amount: +balanceFullWallet - +amountEvent
-            })
-        }
-    } catch (e) { console.log(e) }
-}
-
-export const recordingAllEvents = async (type: string) => {
-    try {
-        const contract = await createContract();
-        const events = await contract.getPastEvents(type, {
-            fromBlock: 0,
-            toBlock: 'latest',
-        })
-
-        for (let event of events) {
-            const { account, amount, token, addressOwner, addressToken } = event.returnValues
-            const amountWallet = Web3.utils.fromWei(amount, 'ether')
-            await Transaction.createTransaction({
-                idTransaction: event.id,
-                addressOwner: (event.event === 'Deposit') ? addressOwner : account,
-                amount: amountWallet,
-                addressToken: (event.event === "Deposit") ? addressToken : token,
-                event: event.event
-            })
-        }
-
     } catch (e) { console.log(e) }
 }
